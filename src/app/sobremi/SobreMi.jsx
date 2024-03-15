4047044c21d7b8ad02b28afabce2c1b120875b8d
Alan opresnik <alanopk328@gmail.com>
2024-03-15 18:25:09 -0300

    fix all

--- src/app/sobremi/SobreMi.jsx
+++ src/app/sobremi/SobreMi.jsx
@@ -2,31 +2,25 @@
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import React, { useEffect } from "react";
+import { motion } from "framer-motion";
 
-import ScrollReveal from 'scrollreveal';
+
+
 import "swiper/css";
 import "swiper/css/pagination";
 
 import { Pagination } from "swiper/modules";
 
 const SobreMi = () => {
-    useEffect(() => {
-        const sr = ScrollReveal();
-        sr.reveal(".familia", {
-            duration: 1000,
-            distance: "50px",
-        });
-        const sr2 = ScrollReveal();
-        sr.reveal(".btn", {
-            duration: 2000,
-            distance: "50px",
-            origin: "left",
-            delay: 300,
-        });
-    }, []);
 
     return (
-        <section className="text-gray-600 body-font mt-[8rem] md:mt-[8rem] max-w-[1250px] overflow-hidden">
+        <motion.div
+            initial={{ opacity: 0, x: -100 }} // Configuración inicial: opacidad 0 y posición x -100 (fuera de la pantalla hacia la izquierda)
+            animate={{ opacity: 1, x: 0 }} // Animación de fade in y movimiento hacia la derecha
+            transition={{ duration: 1 }} // Duración de la animación (1 segundo)
+
+
+            className="text-gray-600 body-font mt-[8rem] md:mt-[8rem] max-w-[1250px] overflow-hidden">
             <div className="familia border-b-2 container mx-auto flex px-5 py-6 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-6 items-center text-center">
                     <h3 className="title-font border-b-2 border-[#a7967f] w-fit sm:text-4xl text-4xl mb-1 font-medium text-gray-900">
@@ -54,7 +48,7 @@ const SobreMi = () => {
                 <Button as={Link} href="https://wa.me/+5491128736549" className="btn" color="primary">Enviar un mensaje</Button>
             </div>
 
-        </section>
+        </motion.div>
     );
 };
 
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,6 +1,4 @@
 "use client"
-
-import axios from 'axios';
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
 const ContactForm = () => {
